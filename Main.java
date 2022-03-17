3e64f20df89f377ae261b56b85b0bf627bdc5d87
Taisei Iwai <t.iwai.sys21@morijyobi.ac.jp>
2022-03-17 12:12:57 +0900

    Main.java作成

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+ public static void main(String[] args) {
+      double BMI, height, weight;
+      height = 1.7;
+      weight = 56.0;
+      BMI = weight / height/height;
+      System.out.println(BMI);  
+    }
+}

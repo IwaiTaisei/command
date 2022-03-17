351de43b1e90d874158ef04be3bd538d599c71a0
Taisei Iwai <t.iwai.sys21@morijyobi.ac.jp>
2022-03-17 12:23:42 +0900

    Main.javaに2回繰り返しを追加"

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 public class Main {
  public static void main(String[] args) {
+     for(int i=1; i <= 2; i++){
       double BMI, height, weight;
       height = 1.7;
       weight = 56.0;
       BMI = weight / height/height;
-      System.out.println(BMI);  
+      System.out.println(BMI); 
+     } 
     }
 }
